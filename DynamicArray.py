e8fb35657522abdd390d1fbe9cb321bc8c047204
WzhRslh <38376151+wzhSteve@users.noreply.github.com>
2022-05-13 13:04:37 +0800

    Update DynamicArray.py

--- DynamicArray.py
+++ DynamicArray.py
@@ -1,222 +1,171 @@
-import math
+import copy
 
 
 class DynamicArray:
-    def __init__(self, capacity=64, load_factor=2):
-        self.capacity = capacity
-        self.load_factor = load_factor
-        self.array_node = []
-        self.pos = 0
+    def __init__(self, capacity=10, factor=2):
+        self.__capacity = capacity
+        self.__factor = factor
+        self.__chunk = [None] * self.__capacity
+        self.__element_index = -1
+        self.__length = 0
+
+    def length(self):
+        return self.__length
+
+    def add(self, ele):
+        if self.__length == self.__capacity:
+            new_chunk = int(self.__capacity * self.__factor) - self.__capacity
+            self.__chunk += [None] * new_chunk
+            self.__capacity = self.__capacity + new_chunk
+        self.__chunk[self.__length] = ele
+        self.__length += 1
 
     def __str__(self):
-        """for str() implementation for printing"""
-        deli = ", "
-        res = ""
-        if len(self.array_node) != 0:
-            nodes = self.array_node[::-1]
-            res = deli.join(map(str, nodes))
-        return "[" + res + "]"
+        return str(self.__chunk[:self.__length])
 
     def __eq__(self, other):
-        """prove that two DynamicArray are same"""
-        len1 = len(self.array_node)
-        len2 = len(other.array_node)
+        len1 = self.length()
+        len2 = other.length()
         if len1 != len2:
             return False
-        else:
-            if len1 == 0:
-                return True
-            for i in range(len1):
-                node1 = self.array_node[i]
-                node2 = other.array_node[i]
-                flag1 = ((type(node1) == int
-                          or type(node1) == float)
-                         and math.isnan(node1))
-                flag2 = ((type(node2) == int
-                          or type(node2) == float)
-                         and math.isnan(node2))
-                if flag1 is True and flag2 is True:
-                    continue
-                if self.array_node[i] != other.array_node[i]:
-                    return False
+        elif len1 == 0:
             return True
+        for i, j in zip(self, other):
+            if i != j:
+                return False
+        return True
+
+    def __iter__(self):
+        return Iterator(self.__chunk, self.__length)
 
 
-def cons(a, b):
-    if isinstance(b, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    dynamicArray = DynamicArray()
-    capacity = b.capacity
-    load_factor = b.load_factor
-    if len(b.array_node) == b.capacity:
-        capacity = b.capacity * load_factor
-    dynamicArray.capacity = capacity
-    dynamicArray.load_factor = load_factor
-    dynamicArray.array_node.extend(b.array_node)
-    dynamicArray.array_node.append(a)
-    return dynamicArray
+class Iterator(object):
+    def __init__(self, lst, lng):
+        self.__chunk = lst
+        self.__length = lng
+        self.__element_index = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.__element_index += 1
+        if self.__element_index >= self.__length:
+            raise StopIteration()
+        return self.__chunk[self.__element_index]
 
 
-def remove(a, b):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    dynamicArray = DynamicArray()
-    c = (type(b) == int or type(b) == float) and math.isnan(b)
-    for ele in a.array_node:
-        if c is True and math.isnan(ele):
-            continue
-        elif ele != b:
-            dynamicArray.array_node.append(ele)
-    dynamicArray.capacity = a.capacity
-    return dynamicArray
+def cons(lst, ele):
+    tmp = copy.deepcopy(lst)
+    tmp.add(ele)
+    return tmp
 
 
-def length(a):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    return len(a.array_node)
+def remove(lst, pos):
+    if pos < 0 or pos >= lst.length():
+        raise Exception('The location accessed is not in the array!')
+    tmp = DynamicArray(capacity=len(lst1))
+    for i, _ in enumerate(lst):
+        if i != pos:
+            tmp.add(_)
+    return tmp
 
 
-def isMember(a, b):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    nan_flag = (type(b) == int or type(b) == float) and math.isnan(b)
-    for ele in a.array_node:
-        if ele == b:
-            return True
-        elif nan_flag and math.isnan(ele):
+def length(lst):
+    return lst.length()
+
+
+def member(lst, element):
+    for _ in lst:
+        if element == _:
             return True
     return False
 
 
-def reverse(a):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    dynamicArray = DynamicArray()
-    tem_node = a.array_node
-    tem_node.reverse()
-    dynamicArray.array_node = tem_node
-    dynamicArray.capacity = a.capacity
-    return dynamicArray
-
-
-def to_list(a):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    result = []
-    result.extend(a.array_node)
-    result.reverse()
-    return result
-
-
-def from_list(a):
-    if isinstance(a, list) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    dynamicArray = DynamicArray()
-    node = []
-    node.extend(a)
-    node.reverse()
-    # the original list in the DynamicArray is reversed
-    # so, when we get a list to the DynamicArray
-    # we should reverse it.
-    dynamicArray.array_node.extend(node)
-    dynamicArray.capacity = len(a) * dynamicArray.load_factor
-    return dynamicArray
-
-
-def find(a, f):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    result = []
-    for ele in a.array_node:
-        if f(ele) is True:
-            result.append(ele)
-    result.reverse()
-    return result
-
-
-def filter_the_value(a, f):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    dynamicArray = DynamicArray()
-    for ele in a.array_node:
-        if f(ele) is True:
-            dynamicArray.array_node.append(ele)
-    siz = len(dynamicArray.array_node)
-    factor = dynamicArray.load_factor
-    dynamicArray.capacity = siz * factor
-    return dynamicArray
-
-
-def map_the_value(a, f):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    dynamicArray = DynamicArray()
-    dynamicArray.capacity = a.capacity
-    for ele in a.array_node:
-        dynamicArray.array_node.append(f(ele))
-    return dynamicArray
-
-
-def reduce(a, f, initial_state):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    state = initial_state
-    dynamicArray = DynamicArray()
-    dynamicArray.capacity = a.capacity
-    for ele in a.array_node:
-        dynamicArray.array_node.append(f(ele, state))
-    return dynamicArray
-
-
-def iterator_element(a):
-    if isinstance(a, DynamicArray) is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    dynamicArray = DynamicArray()
-    dynamicArray.capacity = a.capacity
-    dynamicArray.array_node.extend(a.array_node)
-    return dynamicArray
-
-
-def next_element(a):
-    if a.pos > len(a.array_node):
-        print("stop iterator")
-        return 0
-    siz = len(a.array_node) - 1
-    tmp = a.array_node[siz - a.pos]
-    a.pos += 1
+def from_list(v):
+    tmp = DynamicArray()
+    for _ in v:
+        tmp.add(_)
     return tmp
 
 
-def empty(a):
-    return DynamicArray()
+def reverse(lst):
+    tmp = []
+    for _ in lst:
+        tmp.append(_)
+    return from_list(tmp[::-1])
 
 
-def concat(a, b):
-    flag1 = isinstance(a, DynamicArray)
-    flag2 = isinstance(b, DynamicArray)
-    if flag1 is False or flag2 is False:
-        print("Please input the correct DynamicArray object.")
-        return DynamicArray()
-    dynamicArray = DynamicArray()
-    len1 = len(a.array_node)
-    len2 = len(b.array_node)
-    factor = dynamicArray.load_factor
-    dynamicArray.capacity = (len1 + len2) * factor
-    node = b.array_node
-    dynamicArray.array_node.extend(node)
-    node = a.array_node
-    dynamicArray.array_node.extend(node)
-    return dynamicArray
+def to_list(lst):
+    res = []
+    for _ in lst:
+        res.append(_)
+    return res
+
+
+def find(lst, f):
+    for _ in lst:
+        if f(_):
+            return True
+    return False
+
+
+def filter(f, lst):
+    res = DynamicArray(capacity=lst.length())
+    for k in lst:
+        if f(k):
+            res.add(k)
+    return res
+
+
+def map(f, *iters):
+    res = DynamicArray()
+    for args in zip(*iters):
+        res.add(f(*args))
+    return res
+
+
+def reduce(function, lst, initializer=None):
+    it = iterator(lst)
+    if initializer is None:
+        try:
+            value = next(it)
+        except StopIteration:
+            raise TypeError("reduce() of empty sequence with no "
+                            "initial value") from None
+    else:
+        value = initializer
+    for element in it:
+        value = function(value, element)
+    return value
+
+
+def iterator(lst):
+    return iter(lst)
+
+
+def empty():
+    return DynamicArray(capacity=0)
+
+
+def concat(lst1, lst2):
+    res = DynamicArray(capacity=lst1.length()+lst2.length())
+    for k in lst1:
+        res.add(k)
+    for k in lst2:
+        res.add(k)
+    return res
+
+
+def set(lst, p, v):
+    if v is not None and type(v) != int:
+        raise Exception('Input data must be int or None')
+    if p < 0 or p >= lst.length():
+        raise Exception('The location accessed is not in the array!')
+    res = DynamicArray(capacity=lst.length())
+    for idx, i in enumerate(lst):
+        if p == idx:
+            res.add(v)
+        else:
+            res.add(i)
+    return res
